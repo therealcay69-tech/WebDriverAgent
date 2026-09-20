95e13d8f2c7ca91f1d1dd701ded3762cb3ba58cf
WDA Photos Experiment <wda-photos-experiment@localhost>
2026-09-20 20:56:58 +0300

    Fix PhotoKit command compilation

--- WebDriverAgentLib/Commands/FBPhotoCommands.m
+++ WebDriverAgentLib/Commands/FBPhotoCommands.m
@@ -24,45 +24,20 @@
 
   if (![filePath isKindOfClass:[NSString class]] || filePath.length == 0) {
     return FBResponseWithStatus(
-      [FBCommandStatus invalidArgumentWithMessage:@"Missing filePath"
+      [FBCommandStatus invalidArgumentErrorWithMessage:@"Missing filePath"
                                          traceback:nil]
     );
   }
 
   if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
     return FBResponseWithStatus(
-      [FBCommandStatus invalidArgumentWithMessage:
+      [FBCommandStatus invalidArgumentErrorWithMessage:
         [NSString stringWithFormat:@"File does not exist: %@", filePath]
                                          traceback:nil]
     );
   }
 
   NSURL *fileURL = [NSURL fileURLWithPath:filePath];
-
-  PHAuthorizationStatus status =
-    [PHPhotoLibrary authorizationStatusForAccessLevel:PHAccessLevelAddOnly];
-
-  if (status == PHAuthorizationStatusNotDetermined) {
-    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-
-    [[PHPhotoLibrary sharedPhotoLibrary]
-      requestAuthorizationForAccessLevel:PHAccessLevelAddOnly
-      handler:^(PHAuthorizationStatus newStatus) {
-        status = newStatus;
-        dispatch_semaphore_signal(semaphore);
-      }];
-
-    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-  }
-
-  if (status != PHAuthorizationStatusAuthorized &&
-      status != PHAuthorizationStatusLimited) {
-    return FBResponseWithStatus(
-      [FBCommandStatus unknownErrorWithMessage:
-        @"Photos access was not authorized"
-                                         traceback:nil]
-    );
-  }
 
   __block PHObjectPlaceholder *placeholder = nil;
   __block NSError *changeError = nil;
